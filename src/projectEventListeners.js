32dbec89e49472a79e8e04bc88e231ba761879f5
carlDguez19 <carldominguez19@gmail.com>
2023-11-29 18:39:58 -0700

    very basic variables for project event listeners

---
+++ src/projectEventListeners.js
@@ -0,0 +1,7 @@
+import { dqs } from "./menuEventListeners";
+import { projArr } from "./menuEventListeners";
+
+const projTaskAdd = dqs(".projAdd");
+const projEditButton = dqs(".projEdit");
+const projRemButton = dqs(".projRemove");
+
