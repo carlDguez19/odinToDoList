8151f5912b6153ab340ca9e843449abd802b18f3
carlDguez19 <carldominguez19@gmail.com>
2024-01-10 19:15:53 -0700

    fixed the styling of the task and the way it is displayed. Added the buttons to remove and edit the task. After finishing the edit project button I will move onto these buttons. This should be the last thing I work on before I start implementing date-fns and organizing tasks into the tabs on the side by date. And using memory to store tasks semi permanently

--- src/taskDOM.js
+++ src/taskDOM.js
@@ -1,8 +1,13 @@
+import Edit from '../src/imgs/edit.png';
+import Remove from '../src/imgs/close.png';
+
 export function displayTaskInMain(task){//(project, ul) might be needed as params but probs not
     const projUL = document.querySelector(".todoUL");
 
     const taskLI = document.createElement('li');
     //taskLI.classList.add("todo");
+
+    const buttonDiv = document.createElement('div');
 
     const taskCheckbox = document.createElement('input');
     taskCheckbox.type = "checkbox";
@@ -12,9 +17,30 @@ export function displayTaskInMain(task){//(project, ul) might be needed as param
     taskDateInput.type = "date";
     //taskDateInput.classList.add("todoDate");
 
+    const taskEditDiv = document.createElement('div');
+    const taskRemoveDiv = document.createElement('div');
+
+    const taskEditIcon = new Image();
+    taskEditIcon.src = Edit;
+    taskEditIcon.classList.add('projEdit');
+
+    const taskRemoveIcon = new Image();
+    taskRemoveIcon.src = Remove;
+    taskRemoveIcon.classList.add('projRemove');
+
+    taskEditDiv.appendChild(taskEditIcon);
+    taskRemoveDiv.appendChild(taskRemoveIcon);
+
     taskLI.textContent = task.tTitle + "  " + task.tDue;
     taskLI.appendChild(taskDateInput);
-    taskLI.appendChild(taskCheckbox);
+
+    buttonDiv.appendChild(taskCheckbox);
+    buttonDiv.appendChild(taskEditDiv);
+    buttonDiv.appendChild(taskRemoveDiv);
+
+    buttonDiv.classList.add('taskButtons');
+
+    taskLI.appendChild(buttonDiv);
 
     taskLI.classList.add("todo");
 
